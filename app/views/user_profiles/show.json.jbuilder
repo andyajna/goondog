54ce6ac6deb0e802b945f05fe7f20aa2d5043e91
Andrew Kemp <andrewkemp70@gmail.com>
2013-11-02 16:17:03 +1100

    First commit

---
+++ app/views/user_profiles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user_profile, :profile_pic, :hobbies, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Goondog::Application.routes.draw do
+  resources :groups do
+  	member do
+  		get :leave
+      get :join
+      get :remove_user
+  	end
+  end
+
+  resources :user_profiles
+
+  resources :posts
+
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
