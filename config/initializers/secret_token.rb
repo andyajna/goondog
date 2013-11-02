235bbe38fd365aac8e3b4ff7efe742628e70177e
Andrew Kemp <andrewkemp70@gmail.com>
2013-11-02 11:54:34 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goondog::Application.config.secret_key_base = '7692549058ce2c55c69d437d343f5ed5d50d1aa4e83f53ce69e13df2109e7b6a3fd5ec748183dec84b565bcb732bf2f7577a00732232b594c73805c1dea61383'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Goondog::Application.config.session_store :cookie_store, key: '_goondog_session'
